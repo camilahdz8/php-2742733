27ac698c450e745ef4fb880c988796a532f305ed
Camilahdz <mariacamilah23408@gmail.com>
2024-05-10 15:03:59 -0500

    v2

---
+++ 01-sintaxis-variables/index.php
@@ -0,0 +1,22 @@
+<?php
+echo 'Ejecutando sitaxis variable..';
+?>
+
+<!-- <?php
+/* ejercicio 1 */
+$nombre_1 = "Pericles";
+$nombre_2 = "Camila";
+
+echo " Hola $nombre_1 y $nombre_2, ¿Como estan? <br> <br> Pagina creada por: <br> <br> - $nombre_1 <br><br> -$nombre_2 <br><br>";
+?>
+<?php
+$mi_numero = 8;
+$respuesta = $mi_numero;
+$respuesta += 2;
+$respuesta *= 2;
+$respuesta -= 2;
+$respuesta /= 2;
+$respuesta -= $mi_numero;
+
+echo $respuesta
+?>
---
+++ 02-condicionales/index.php
@@ -0,0 +1,20 @@
+<?php
+echo 'Ejecutando Condicional'
+?>
+
+<?php
+$Articulos= "20";
+
+if ($Articulos <12) {
+    echo "Caja Rapida";
+} else {
+    echo "Caja Normal";
+}
+
+/*ejercicio 2*/
+
+$Edad = "100";
+
+
+
+?>

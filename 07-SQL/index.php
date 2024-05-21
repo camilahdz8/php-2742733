2681050d16c0a0fc36a0a0b03451368c3ac5e932
Camilahdz <mariacamilah23408@gmail.com>
2024-05-21 16:22:02 -0500

    feat:SQL

---
+++ 07-SQL/index.php
@@ -0,0 +1,20 @@
+<?php
+//Documentacion: SQL W3Schools
+//1. SQL Select 
+//SELECT materia FROM profesores;
+//2. SQL SELECT DISTINCT `Estado civil` FROM profesores;
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>

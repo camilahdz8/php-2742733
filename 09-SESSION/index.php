201966f58f2dd5f0fea072b79949acbaaaf6b07d
Camilahdz <mariacamilah23408@gmail.com>
2024-06-04 16:14:08 -0500

    feat:estilos

--- 09-SESSION/index.php
+++ 09-SESSION/index.php
@@ -55,6 +55,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./style.css">
     
 </head>
 
@@ -63,6 +64,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <div class="container" >
 
         <div class="container_todo">
+
             <h1>Página de inicio</h1>
 
             <form action="index.php" method="POST">
